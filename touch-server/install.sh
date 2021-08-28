7de4c6374fefebd350aa5a97419abe0847fc86b5
Sorakime <69241694+Sorakime@users.noreply.github.com>
2021-08-28 09:42:30 +0900

    Create install.sh

---
+++ touch-server/install.sh
@@ -0,0 +1,20 @@
+echo "Thanks for using this shellscript and Touch Server."
+echo "It works install Touch Server."
+echo "Now, It install Touch Server."
+
+echo "-------------"
+
+echo "Download Zip archive and unzip..."
+wget https://github.com/Sorakime/Touch-Server/archive/refs/heads/main.zip
+unzip main.zip
+echo "Downloaded and completed for unzip"
+
+echo "-------------"
+
+echo "Install mime-types from npm..."
+npm install mime-types
+echo "Installed mime-types from npm"
+
+echo "-------------"
+
+echo "Installed Touch Server."
